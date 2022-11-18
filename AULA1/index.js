099db23dfa49e1df341402cc12b4d7c397795b13
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-17 18:09:57 -0600

    Se inicializaron por defaul en cero y vacio los objetos de la clase de cuentaCorriente

--- AULA1/index.js
+++ AULA1/index.js
@@ -1,36 +1,26 @@
+
+//Definición de clases.
 class cliente {
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
+    rutCliente
 }
-const cliente1 = new cliente();
-cliente1.nombreCliente = "José";
-cliente1.dniCliente = "12354";
-cliente1.numeroCuenta = "1231234";
-cliente1.saldoCuenta = 1500;
 
-const cliente2 = new cliente();
-cliente2.nombreCliente = "Leonardo";
-cliente2.dniCliente = "75984";
-cliente2.numeroCuenta = "84436363";
-cliente2.saldoCuenta = 5400;
+class cuentaCorriente{
+    numero;
+    saldo;
+    agencia;
 
-const cliente3 = new cliente();
-cliente3.nombreCliente = "Gerardo";
-cliente3.dniCliente = "48965";
-cliente3.numeroCuenta = "7498563";
-cliente3.saldoCuenta = 1500;
+    constructor() {
+        this.saldo = 0;
+        this.numero = '';
+        this.agencia = '';
+    }
 
-const nombreCliente = "Leonardo";
-const dniCliente = "13804050";
-const numeroCuenta = "123434343";
-const saldoCuenta = 1000;
-
-const nombreCliente2 = "Leonardo";
-const dniCliente2 = "13804050";
-const numeroCuenta2 = "123434343";
-const saldoCuenta2 = 1000;
-
-console.log(nombreCliente,dniCliente, numeroCuenta, saldoCuenta);
-console.log(cliente1, cliente2, cliente3);
+    depositoCuenta(valor){
+        this.saldo += valor;
+    }
+    retirarCuenta(valor){
+        this.saldo -= valor;
+    }
+}
