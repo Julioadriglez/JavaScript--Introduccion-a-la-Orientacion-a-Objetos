295e7c8ce9b4c7c2b90061f6f2b23d5f8b65fcee
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-18 18:06:52 -0600

    se agrego cliente dentro de cuentaDeLeonardo

--- AULA1/index.js
+++ AULA1/index.js
@@ -1,8 +1,19 @@
 //Importacion de clases
+import {cliente} from '../cliente.js';
 import {cuentaCorriente} from '../cuentaCorriente.js';
-import {cliente} from '../cliente.js'
+
+
+const Cliente = new cliente();
+Cliente.nombreCliente = 'Leonardo';
+Cliente.dniCliente = '12345666';
+Cliente.rutCliente = '15666';
 
 const cuentDeLeonardo = new cuentaCorriente();
+cuentDeLeonardo.numero = '1';
+cuentDeLeonardo.agencia = '001';
+cuentDeLeonardo.cliente = Cliente;
+
+console.log(cuentDeLeonardo);
 
 let saldo = cuentDeLeonardo.verSaldo();
 console.log('El saldo actual: '+saldo);
