19c1d9adcaf6d9666ffcf943fe259ada040f535c
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-19 18:15:55 -0600

    Se mejoro la estructura del constructor de la clase Cliente

--- cliente.js
+++ cliente.js
@@ -1,7 +1,14 @@
 //definición de clases
 
-export class cliente {
+export class Cliente {
     nombreCliente;
     dniCliente;
     rutCliente;
+
+    constructor(nombreCliente, dniCliente, rutCliente){
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.rutCliente = rutCliente;
+
+    }
 }
