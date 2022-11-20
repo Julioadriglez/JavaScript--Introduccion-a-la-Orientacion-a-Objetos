402631d1fd9755d628cb356e869d9a47e85972cf
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-19 19:01:16 -0600

    obteniendo datos de cuenta corrientes

--- AULA1/index.js
+++ AULA1/index.js
@@ -6,33 +6,15 @@ import {cuentaCorriente} from '../cuentaCorriente.js';
 const cliente = new Cliente('Leonardo', '12345666', '15666');
 const cliente2 = new Cliente('Maria', '66145431', '44565');
 
-const cuentaDeLeonardo = new cuentaCorriente();
-cuentaDeLeonardo.numero = '1';
-cuentaDeLeonardo.agencia = '001';
-cuentaDeLeonardo.cliente = cliente;
+console.log(cliente);
+console.log(cliente2);
 
-console.log(cuentaDeLeonardo.cliente);
+const cuentaDeLeonardo = new cuentaCorriente(cliente, '1', '001');
+const cuentaMaria = new cuentaCorriente(cliente2, '2','002');
 
 let saldo = cuentaDeLeonardo.verSaldo();
 
 saldo = cuentaDeLeonardo.depositoCuenta(100);
-console.log('el saldo actual es (cuentaDeLeonardo) :'+saldo);
 
-const cuentaMaria = new cuentaCorriente();
-cuentaMaria.numero = '2';
-cuentaMaria.agencia = '002';
-cuentaMaria.cliente = cliente2;
+console.log(cuentaCorriente.cantidadCuentas);
 
-console.log(cuentaMaria.cliente);
-
-cuentaMaria.cliente = 0;
-
-console.log(cuentaMaria.cliente);
-/* cuentaDeLeonardo.transferirParaCuenta(100,cuentaMaria)
-
-const saldoMaria = cuentaMaria.verSaldo();
-console.log('El saldo actual (cuentaMaria) '+saldoMaria)
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('el saldo actual es (cuentaDeLeonardo) :'+saldoLeonardo);
- */
