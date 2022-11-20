aafca5b418aeac039f0d518c5adecd28e229df2a
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-19 18:17:18 -0600

    Se implemento los constructores de la clase cuentaCorriente crendo las cuentas

--- AULA1/index.js
+++ AULA1/index.js
@@ -1,40 +1,38 @@
 //Importacion de clases
-import {cliente} from '../cliente.js';
+import {Cliente} from '../cliente.js';
 import {cuentaCorriente} from '../cuentaCorriente.js';
 
 
-const Cliente = new cliente();
-Cliente.nombreCliente = 'Leonardo';
-Cliente.dniCliente = '12345666';
-Cliente.rutCliente = '15666';
+const cliente = new Cliente('Leonardo', '12345666', '15666');
+const cliente2 = new Cliente('Maria', '66145431', '44565');
 
 const cuentaDeLeonardo = new cuentaCorriente();
 cuentaDeLeonardo.numero = '1';
 cuentaDeLeonardo.agencia = '001';
-cuentaDeLeonardo.cliente = Cliente;
+cuentaDeLeonardo.cliente = cliente;
 
-console.log(cuentaDeLeonardo);
+console.log(cuentaDeLeonardo.cliente);
 
 let saldo = cuentaDeLeonardo.verSaldo();
 
 saldo = cuentaDeLeonardo.depositoCuenta(100);
 console.log('el saldo actual es (cuentaDeLeonardo) :'+saldo);
 
-
-const cliente2 = new cliente();
-cliente2.nombreCliente = 'Maria';
-cliente2.dniCliente = '66145431';
-cliente2.rutCliente = '44565';
-
 const cuentaMaria = new cuentaCorriente();
-cuentaMaria.numero = '';
+cuentaMaria.numero = '2';
 cuentaMaria.agencia = '002';
-cuentaMaria.cliente = Cliente;
+cuentaMaria.cliente = cliente2;
 
-cuentaDeLeonardo.transferirParaCuenta(100,cuentaMaria)
+console.log(cuentaMaria.cliente);
+
+cuentaMaria.cliente = 0;
+
+console.log(cuentaMaria.cliente);
+/* cuentaDeLeonardo.transferirParaCuenta(100,cuentaMaria)
 
 const saldoMaria = cuentaMaria.verSaldo();
 console.log('El saldo actual (cuentaMaria) '+saldoMaria)
 
 const saldoLeonardo = cuentaDeLeonardo.verSaldo();
 console.log('el saldo actual es (cuentaDeLeonardo) :'+saldoLeonardo);
+ */
