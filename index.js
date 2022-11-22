13860ed298727e2808c651ec909481da6f145cb4
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-22 16:14:34 -0600

    se agregaron de nuevo cuentaCorrienta y CuentAhorro

---
+++ index.js
@@ -0,0 +1,20 @@
+//Importacion de clases
+import {Cliente} from './cliente.js';
+import {CuentaAhorro} from './CuentaAhorro.js';
+import {cuentaCorriente} from './cuentaCorriente.js';
+
+const cliente = new Cliente('Leonardo', '12345666', '15666');
+const cliente2 = new Cliente('Maria', '66145431', '44565');
+
+const cuentaDeLeonardo = new cuentaCorriente('Corriente', cliente, '1', '001');
+const cuentaMaria = new cuentaCorriente('Corriente', cliente2, '2','002');
+
+const cuentaAhorroDeLeonardo = new CuentaAhorro('Ahorro', cliente,'9985', '001', 0);
+
+console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoCuenta(150);
+cuentaDeLeonardo.retirarCuenta(70);
+console.log(cuentaDeLeonardo.verSaldo())
+console.log(cuentaAhorroDeLeonardo);
+
+
