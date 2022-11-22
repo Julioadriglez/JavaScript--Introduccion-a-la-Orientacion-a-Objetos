0dee63d3fbd3085426860131c68c83423ff49094
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-22 16:06:59 -0600

    se agrego el set y get de cliente en Cuentas.js

--- Cuenta.js
+++ Cuenta.js
@@ -1,3 +1,41 @@
 export class Cuenta {
-    
+    #cliente;
+    #saldo;
+    constructor(tipo,cliente, numero, agencia, saldo) {
+        this.tipo = tipo;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.#cliente = cliente;
+        this.#saldo = saldo;
+    }
+
+    set cliente(valor){
+        if(valor instanceof Cliente)
+        this.#cliente = valor;
+    }
+
+    get cliente(){
+        return this.#cliente;
+    }
+
+    depositoCuenta(valor){
+        if(valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarCuenta(valor){
+        if(valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo () {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor, cuentaDestino){
+        this.retirarCuenta(valor);
+        cuentaDestino.depositoCuenta(valor);
+    }
 }
