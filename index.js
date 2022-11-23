2b7ef81764db2eb893f09e420b607d9757d2302a
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-23 14:47:36 -0600

    Implementacion de ejmplos con CuentaNomina y agregar modulos de esta msima

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 //Importacion de clases
 import {Cliente} from './cliente.js';
+import { Cuenta } from './Cuenta.js';
 import {CuentaAhorro} from './CuentaAhorro.js';
 import {cuentaCorriente} from './cuentaCorriente.js';
+import { CuentaNomina } from './CuentaNomina.js';
 
 const cliente = new Cliente('Leonardo', '12345666', '15666');
 const cliente2 = new Cliente('Maria', '66145431', '44565');
 
-const cuentaDeLeonardo = new cuentaCorriente('Corriente', cliente, '1', '001');
-const cuentaMaria = new cuentaCorriente('Corriente', cliente2, '2','002');
+const cuentaDeLeonardo = new cuentaCorriente( cliente, '1', '001');
+const cuentaMaria = new cuentaCorriente(cliente2, '2','002');
 
-const cuentaAhorroDeLeonardo = new CuentaAhorro('Ahorro', cliente,'9985', '001', 0);
+const cuentaAhorroDeLeonardo = new CuentaAhorro(cliente,'9985', '001', 0);
 
-console.log(cuentaDeLeonardo);
-cuentaDeLeonardo.depositoCuenta(150);
-cuentaDeLeonardo.retirarCuenta(70);
-console.log(cuentaDeLeonardo.verSaldo())
-console.log(cuentaAhorroDeLeonardo); 
+const cuentaNominaLeonardo = new CuentaNomina(cliente, '098', '001',0)
 
+cuentaNominaLeonardo.depositoCuenta(150);
+console.log(cuentaNominaLeonardo);
+
+cuentaNominaLeonardo.retirarCuenta(50);
+console.log(cuentaNominaLeonardo.verSaldo())
 
 
