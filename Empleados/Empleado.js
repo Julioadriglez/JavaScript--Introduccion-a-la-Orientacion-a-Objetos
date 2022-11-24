e255a1522a5bcfba571a2da70b9644158d4f612a
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-11-23 19:27:07 -0600

    implementacion de polimorfismo en validacion y cliente

--- Empleados/Empleado.js
+++ Empleados/Empleado.js
@@ -2,10 +2,24 @@ export class Empleado {
     #nombre;
     #dni;
     #salario;
+    #clave;
     constructor(nombre, dni, salario) {
         this.#nombre = nombre;
         this.#dni = dni;
         this.#salario = salario;
+        this.#clave = '';
+    }
+
+    asignarClave(clave){
+        this.#clave = clave;
+    }
+
+    autenticable(clave){
+        return clave == this.#clave;
+
+    }
+    get clave() {
+        return this.#clave;
     }
 
     verBonificacion(){
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //Importacion de clases
 import { Cliente } from './cliente.js';
-import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
-import { cuentaCorriente } from './Cuentas/cuentaCorriente.js';
-import { CuentaNomina } from './Cuentas/CuentaNomina.js';
 import { Director } from './Empleados/Director.js';
 import { Empleado } from './Empleados/Empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 
-const cliente = new Cliente('Leonardo', '12345666', '15666');
-const cliente2 = new Cliente('Maria', '66145431', '44565');
+//const cliente2 = new Cliente('Maria', '66145431', '44565');
 
 const empleado = new Empleado('juan Perez', '456982', 10000);
+empleado.asignarClave('12345');
+console.log(SistemaAutenticacion.login(empleado, '12345'))
 const gerente = new Gerente('Pedro Rivas', '459623', 12000);
-const director = new Director('elena Moreno', '4512134', 15000);
-
-console.log(empleado.verBonificacion());
-console.log(gerente.verBonificacion());
-console.log(director.verBonificacion());
+//const director = new Director('elena Moreno', '4512134', 15000);
+empleado.asignarClave('12305');
+console.log(SistemaAutenticacion.login(empleado,'12305'));
+const cliente = new Cliente('Leonardo', '12345666', '15666');
+cliente.asignarClave('11111');
+console.log(SistemaAutenticacion.login(empleado,'11111'));
 
